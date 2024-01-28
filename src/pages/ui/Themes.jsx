29684dc830e85dfab784ac7b4335b3ef03aeb5c3
BrandonRv <138633220+BrandonRv@users.noreply.github.com>
2024-01-28 02:03:51 -0500

    deploy react vite

--- src/pages/ui/Themes.jsx
+++ src/pages/ui/Themes.jsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 
 export function Themes() {
     const themes = localStorage.getItem("themess");
-    const [activeTheme, setActiveTheme] = useState(themes ? themes : '/assets/styles/theme-7.scss');
+    const [activeTheme, setActiveTheme] = useState(themes ? themes : './assets/styles/theme-7.scss');
     const [isPanelOpen, setIsPanelOpen] = useState(false); // Inicialmente cerrado
     const configPanelRef = useRef(null);
     useEffect(() => {
@@ -64,16 +64,16 @@ export function Themes() {
                             <h5 className="panel-title text-center">Fondos</h5>
                             <ul id="color-options" className="list-inline mb-0 panel-inner">
                                 {/* ... opciones de temas ... */}
-                                <li className="theme-1  list-inline-item"><a onClick={handleThemeClick} data-style="/assets/styles/theme-1.scss" href="#"></a></li>
-                                <li className="theme-2  list-inline-item"><a onClick={handleThemeClick} data-style="/assets/styles/theme-2.scss" href="#"></a></li>
-                                <li className="theme-3  list-inline-item"><a onClick={handleThemeClick} data-style="/assets/styles/theme-3.scss" href="#"></a></li>
-                                <li className="theme-4  list-inline-item"><a onClick={handleThemeClick} data-style="/assets/styles/theme-4.scss" href="#"></a></li>
-                                <li className="theme-5  list-inline-item"><a onClick={handleThemeClick} data-style="/assets/styles/theme-5.scss" href="#"></a></li>
-                                <li className="theme-6  list-inline-item"><a onClick={handleThemeClick} data-style="/assets/styles/theme-6.scss" href="#"></a></li>
-                                <li className="theme-7  list-inline-item"><a onClick={handleThemeClick} data-style="/assets/styles/theme-7.scss" href="#"></a></li>
-                                <li className="theme-8  list-inline-item"><a onClick={handleThemeClick} data-style="/assets/styles/theme-8.scss" href="#"></a></li>
-                                <li className="theme-9  list-inline-item"><a onClick={handleThemeClick} data-style="/assets/styles/theme-9.scss" href="#"></a></li>
-                                <li className="theme-10  list-inline-item"><a onClick={handleThemeClick} data-style="/assets/styles/theme-10.scss" href="#"></a></li>
+                                <li className="theme-1  list-inline-item"><a onClick={handleThemeClick} data-style="./assets/styles/theme-1.scss" href="#"></a></li>
+                                <li className="theme-2  list-inline-item"><a onClick={handleThemeClick} data-style="./assets/styles/theme-2.scss" href="#"></a></li>
+                                <li className="theme-3  list-inline-item"><a onClick={handleThemeClick} data-style="./assets/styles/theme-3.scss" href="#"></a></li>
+                                <li className="theme-4  list-inline-item"><a onClick={handleThemeClick} data-style="./assets/styles/theme-4.scss" href="#"></a></li>
+                                <li className="theme-5  list-inline-item"><a onClick={handleThemeClick} data-style="./assets/styles/theme-5.scss" href="#"></a></li>
+                                <li className="theme-6  list-inline-item"><a onClick={handleThemeClick} data-style="./assets/styles/theme-6.scss" href="#"></a></li>
+                                <li className="theme-7  list-inline-item"><a onClick={handleThemeClick} data-style="./assets/styles/theme-7.scss" href="#"></a></li>
+                                <li className="theme-8  list-inline-item"><a onClick={handleThemeClick} data-style="./assets/styles/theme-8.scss" href="#"></a></li>
+                                <li className="theme-9  list-inline-item"><a onClick={handleThemeClick} data-style="./assets/styles/theme-9.scss" href="#"></a></li>
+                                <li className="theme-10  list-inline-item"><a onClick={handleThemeClick} data-style="./assets/styles/theme-10.scss" href="#"></a></li>
                                 {/* ... */}
                             </ul>
                             <a onClick={togglePanel} className="close" href="#">
